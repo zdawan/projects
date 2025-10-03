dad46fe547de19898e5aab63930a7e43382087d5
zdawan <kumarjothi704@gmail.com>
2025-10-03 16:01:17 +0530

    Intial commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -19,14 +19,6 @@ export const categories = [
 
 export const projects = [
   {
-    id: "gold-investment-app",
-    title: "Valura.Ai",
-    year: "December 2025",
-    type: "Mobile App Development",
-    categories: ["/Coming soon"],
-    image: Wireframe,
-  },
-  {
     id: "ai-powered-bug-tracker",
     title: "Bug-Tracker",
     year: "October 2025",
