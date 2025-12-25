dd547f6e8cb3c8c1c561ac91cbe539216a7dbdd1
Dharshan J <jnr1006262@gmail.com>
2025-12-25 19:00:41 +0530

    Filter updated

---
+++ projects.js
@@ -0,0 +1,101 @@
+import Wireframe from "../assets/Wireframe.png";
+import Wireframe0 from "../assets/Wireframe - 0.png";
+import Wireframe1 from "../assets/Wireframe - 2.png";
+import Wireframe2 from "../assets/Wireframe - 3.png";
+import Wireframe3 from "../assets/Wireframe - 4.png";
+import Wireframe4 from "../assets/Wireframe -6.png";
+import Wireframe5 from "../assets/06.png";
+import Wireframe6 from "../assets/Wireframe - 7.png";
+
+export const categories = [
+  "All projects",
+  "/Concepts",
+  "/Strategy",
+  "/UI Design",
+  "/Brand Identity",
+  "/Web Development",
+  "/Coming soon",
+];
+
+export const projects = [
+  {
+    id: "ai-powered-bug-tracker",
+    title: "Bug-Tracker",
+    year: "October 2025",
+    type: "Software Development",
+    categories: ["/Coming soon"],
+    image: Wireframe,
+  },
+  {
+    id: "neopat",
+    title: "Neopat",
+    year: "September 2025",
+    type: "Brand Refinement",
+    categories: ["/Brand Identity", "/Strategy", "/Concepts"],
+    image: Wireframe2,
+    link: "https://www.figma.com/design/T2ElY5rh76lS5SKuZwmz1h/NEO-pat?node-id=0-1&t=gWiXlpjSa46TEwvI-1",
+  },
+  {
+    id: "chirpy",
+    title: "CHIRPY Home Essentials",
+    year: "September 2025",
+    type: "Model Rebrand",
+    categories: ["/Brand Identity", "/Concepts"],
+    image: Wireframe5,
+    link: "https://www.figma.com/design/EsK1RLQ6Y5HOFwYS8w1IJa/Abinavu-project?node-id=0-1&t=JZdWyEs0oNpEeztH-1",
+  },
+  {
+    id: "qr-based-attendance-system",
+    title: "QR-Attendance",
+    year: "June 2025",
+    type: "App Visual Direction",
+    categories: ["/Web Development", "/UI Design"],
+    image: Wireframe0,
+    link: "https://qr-attendance-ten.vercel.app/",
+  },
+  {
+    id: "ai-content-generator",
+    title: "AI-Content-Generator",
+    year: "May 2025",
+    type: "Product Launch",
+    categories: ["/Web Development", "/Strategy", "/Brand Identity"],
+    image: Wireframe1,
+    link: "https://aicontent-generator-dawan18-company.vercel.app/",
+  },
+  {
+    id: "ovi-edu-platform",
+    title: "OviEdu Platform",
+    year: "January 2025",
+    type: "Product UI",
+    categories: ["/Web Development", "/UI Design"],
+    image: Wireframe3,
+    link: "https://www.figma.com/design/xz6iEyBjgqpYMb4vjABdiK/OviEdu-Design?m=auto&t=kFZ122FZrrkkg4Ao-6",
+  },
+  {
+    id: "doctor-patient-app",
+    title: "Doctor / Patient Application",
+    year: "November 2024",
+    type: "Portal Site",
+    categories: ["/Web Development", "/UI Design"],
+    image: Wireframe4,
+    link: "https://www.figma.com/design/NkjJ93PoEMq4Dyzd5MGY68/Mobile-Application-Ui?m=auto&t=yFSLHOU6qk6UZyBv-6",
+  },
+  {
+    id: "jaro",
+    title: "Jaro x IIM Trichy",
+    year: "July 2024",
+    type: "Education Collaboration",
+    categories: ["/Brand Identity", "/UI Design"],
+    image: Wireframe6,
+    link: "https://tinyurl.com/4epfssvu",
+  },
+  // {
+  //   id: "subsquid",
+  //   title: "Subsquid Subscription Platform",
+  //   year: "September 2024",
+  //   type: "Model Rebrand",
+  //   categories: ["/Brand Identity", "/Concepts"],
+  //   image: Wireframe5,
+  //   link: "https://aicontent-generator-dawan18-company.vercel.app/",
+  // },
+];
